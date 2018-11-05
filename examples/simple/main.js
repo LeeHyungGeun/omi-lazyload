de322587ef858b9e78f7e5714dfdf98bd0983004
leehyunggeun <lhg4031@gmail.com>
2018-11-05 22:30:44 +1100

    add example demo

---
+++ examples/simple/main.js
@@ -0,0 +1,59 @@
+import { define, WeElement, render } from 'omi'
+import '../../index'
+
+define('my-app', class extends WeElement {
+  static observe = true
+
+  css() {
+    return `
+      .app {
+        margin: 0 auto;
+      }
+      .empty {
+        width: 100%;
+        height: 100vh;
+        background-color: #e9ecef;
+        text-align: center;
+        vertical-align: middle;
+      }
+      .img {
+        display: flex;
+        justify-content: center;
+      }
+    `
+  }
+
+  render(props, data) {
+    return (
+      <div class="app">
+        <div class="empty">Empty Page</div>
+        <div class="img">
+          <omi-lazyload
+            src="./assets/omi-logo.svg"
+            alt="Omi Logo"
+          />
+        </div>
+        <div class="img">
+          <omi-lazyload
+            src="./assets/omi-logo.svg"
+            alt="Omi Logo"
+          />
+        </div>
+        <div class="img">
+          <omi-lazyload
+            src="./assets/omi-logo.svg"
+            alt="Omi Logo"
+          />
+        </div>
+        <div class="img">
+          <omi-lazyload
+            src="./assets/omi-logo.svg"
+            alt="Omi Logo"
+          />
+        </div>
+      </div>
+    )
+  }
+})
+
+render(<my-app />, '#container')
